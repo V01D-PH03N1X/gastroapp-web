c76225e305e420c6b879ed0ee8dd7312c5d297e0
DarkModz-Official <87519233+DarkModz-Official@users.noreply.github.com>
2022-04-25 13:32:53 +0200

    Add files via upload

---
+++ scripts/reservation.php
@@ -0,0 +1,40 @@
+<?php
+include_once("sql.php");
+
+//Check if Variables are set
+
+if(isset($_POST['name'])){
+    $name = $_POST['name'];
+}
+if(isset($_POST['email'])){
+    $email = $_POST['email'];
+}
+if(isset($_POST['phone'])){
+    $phone = $_POST['phone'];
+}
+if(isset($_POST['date'])){
+    $date = $_POST['date'];
+}
+if(isset($_POST['time'])){
+    $time = $_POST['time'];
+}
+if(isset($_POST['table'])){
+    $table = $_POST['table'];
+}
+//check if table is already reserved
+$sql = "SELECT * FROM reservation WHERE TableID = '$table' AND date = '$date' AND time = '$time'";
+$result = mysqli_query($conn, $sql);
+if(mysqli_num_rows($result) > 0){
+    echo "<h1>Table is already reserved</h1>";
+}
+else{
+    //insert into database
+    $sql = "INSERT INTO reservation (Name, Email, Phone, Date, Time, TableID) VALUES ('$name', '$email', '$phone', '$date', '$time', '$table')";
+    if(mysqli_query($conn, $sql)){
+        echo "<h1>Reservation successful</h1>";
+    }
+    else{
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+}
+?>
---
+++ scripts/sql.php
@@ -0,0 +1,34 @@
+<?php
+//SQL Variables
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "gastroapp";
+
+//Create Connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//Check Connection
+if($conn->connect_error){
+    die("Connection Failed: " . $conn->connect_error);
+}
+
+//Create Table
+$sql = "CREATE TABLE IF NOT EXISTS reservation (
+    `ID` INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `TableID` int NOT NULL,
+    `Name` VARCHAR(255) NOT NULL,
+    `Email` VARCHAR(60) NOT NULL,
+    `Phone` VARCHAR(30) NOT NULL,
+    `Date` date NOT NULL,
+    `Time` time NOT NULL,
+    FOREIGN KEY (TableID) REFERENCES Tables(ID)
+)";
+
+//Check if Table is created
+if($conn->query($sql) === TRUE){
+    echo "<script>console.log('Table Created Successfully');</script>";
+}else{
+    echo "<script>console.log('Error Creating Table: " . $conn->error . "');</script>";
+}
+?>
